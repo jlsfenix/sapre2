51c8e07c2c98d6caae2a297c6443e89e3e662967
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-06-28 14:24:51 -0400

    Replace fields on register page

--- resources/js/Pages/Auth/Register.tsx
+++ resources/js/Pages/Auth/Register.tsx
@@ -1,10 +1,8 @@
 import { useEffect, FormEventHandler } from "react";
 import GuestLayout from "@/Layouts/GuestLayout";
-import InputError from "@/Components/InputError";
-import InputLabel from "@/Components/InputLabel";
-import PrimaryButton from "@/Components/PrimaryButton";
-import TextInput from "@/Components/TextInput";
 import { Head, Link, useForm } from "@inertiajs/react";
+import TextField from "@/Components/TextField";
+import { Button } from "@/Components/ui/button";
 
 export default function Register() {
 	const { data, setData, post, processing, errors, reset } = useForm({
@@ -30,94 +28,80 @@ export default function Register() {
 		<GuestLayout>
 			<Head title="Register" />
 
-			<form onSubmit={submit}>
-				<div>
-					<InputLabel htmlFor="name" value="Name" />
-
-					<TextInput
+			<form className="space-y-6" onSubmit={submit}>
+				<div className="space-y-4">
+					<TextField
 						id="name"
-						name="name"
-						value={data.name}
-						className="mt-1 block w-full"
-						autoComplete="name"
-						isFocused={true}
-						onChange={(e) => setData("name", e.target.value)}
-						required
+						labelProps={{ children: "Nombre" }}
+						inputProps={{
+							name: "name",
+							value: data.name,
+							autoComplete: "name",
+							autoFocus: true,
+							placeholder: "ej: John Doe",
+							onChange: (e) => setData("name", e.target.value),
+							required: true,
+						}}
+						errorMessage={errors.name}
 					/>
 
-					<InputError message={errors.name} className="mt-2" />
-				</div>
-
-				<div className="mt-4">
-					<InputLabel htmlFor="email" value="Email" />
-
-					<TextInput
+					<TextField
 						id="email"
-						type="email"
-						name="email"
-						value={data.email}
-						className="mt-1 block w-full"
-						autoComplete="username"
-						onChange={(e) => setData("email", e.target.value)}
-						required
+						labelProps={{ children: "Email" }}
+						inputProps={{
+							type: "email",
+							name: "email",
+							value: data.email,
+							autoComplete: "username",
+							placeholder: "ej: johndoe@gmail.com",
+							onChange: (e) => setData("email", e.target.value),
+							required: true,
+						}}
+						errorMessage={errors.email}
 					/>
 
-					<InputError message={errors.email} className="mt-2" />
-				</div>
-
-				<div className="mt-4">
-					<InputLabel htmlFor="password" value="Password" />
-
-					<TextInput
+					<TextField
 						id="password"
-						type="password"
-						name="password"
-						value={data.password}
-						className="mt-1 block w-full"
-						autoComplete="new-password"
-						onChange={(e) => setData("password", e.target.value)}
-						required
+						labelProps={{ children: "Contraseña" }}
+						inputProps={{
+							type: "password",
+							name: "password",
+							value: data.password,
+							autoComplete: "new-password",
+							placeholder: "ej: 12345678",
+							onChange: (e) =>
+								setData("password", e.target.value),
+							required: true,
+						}}
+						errorMessage={errors.password}
 					/>
 
-					<InputError message={errors.password} className="mt-2" />
-				</div>
-
-				<div className="mt-4">
-					<InputLabel
-						htmlFor="password_confirmation"
-						value="Confirm Password"
-					/>
-
-					<TextInput
+					<TextField
 						id="password_confirmation"
-						type="password"
-						name="password_confirmation"
-						value={data.password_confirmation}
-						className="mt-1 block w-full"
-						autoComplete="new-password"
-						onChange={(e) =>
-							setData("password_confirmation", e.target.value)
-						}
-						required
-					/>
-
-					<InputError
-						message={errors.password_confirmation}
-						className="mt-2"
+						labelProps={{ children: "Confirmar Contraseña" }}
+						inputProps={{
+							type: "password",
+							name: "password_confirmation",
+							value: data.password_confirmation,
+							placeholder: "ej: 12345678",
+							autoComplete: "new-password",
+							onChange: (e) =>
+								setData(
+									"password_confirmation",
+									e.target.value
+								),
+							required: true,
+						}}
+						errorMessage={errors.password_confirmation}
 					/>
 				</div>
 
 				<div className="flex items-center justify-end mt-4">
-					<Link
-						href={route("login")}
-						className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-					>
-						Already registered?
-					</Link>
+					<Button variant="link">
+						<Link href={route("login")}>¿Ya estas registrado?</Link>
+					</Button>
 
-					<PrimaryButton className="ml-4" disabled={processing}>
-						Register
-					</PrimaryButton>
+					<Button disabled={processing}>Registrar</Button>
 				</div>
 			</form>
 		</GuestLayout>
