435be65d488fab8ef6fb8d351e3944bf918c9160
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-06-29 11:42:24 -0400

    Remove unused components

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,6 +1,5 @@
 import React, { useState, PropsWithChildren, ReactNode } from "react";
 import ApplicationLogo from "@/Components/ApplicationLogo";
-import Dropdown from "@/Components/Dropdown";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
 import { Link } from "@inertiajs/react";
