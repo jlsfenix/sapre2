20c837ee6b61474e33ca5649fa3523b3c68dbdc3
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-07-04 11:45:31 -0400

    Add all methods to UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,4 +16,46 @@ class UserController extends Controller {
 			"users" => User::all(),
 		]);
 	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 */
+	public function create() {
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 */
+	public function store(Request $request) {
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 */
+	public function show(User $role) {
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 */
+	public function edit(User $role) {
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 */
+	public function update(Request $request, User $role) {
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 */
+	public function destroy(User $role) {
+		//
+	}
 }
