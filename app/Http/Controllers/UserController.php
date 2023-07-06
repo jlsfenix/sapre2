a0ba27fd1f65a945e77ae61910fd3fd7b28016fd
ohsolus <yesianlarenas@gmail.com>
2023-07-06 19:25:00 -0400

    User control: Store, update and destroy users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 use Inertia\Inertia;
 use Inertia\Response;
+use Hash;
 
 class UserController extends Controller {
 	/**
@@ -28,6 +30,32 @@ class UserController extends Controller {
 	 * Store a newly created resource in storage.
 	 */
 	public function store(Request $request) {
+		$role = Role::all()->pluck("name");
+
+		$validate = $request->validate([
+			"name" => "required|max:255",
+			"email" => "required|unique:users",
+			"password" => "required",
+			"role" => "required|exists:roles,name",
+		]);
+
+		//creación del usuario
+		$user = User::create([
+			"name" => $validate["name"],
+			"email" => Hash::make($validate["email"]),
+			"password" => $validate["password"],
+		]);
+
+		//asignación de los permisos
+		$user->roles()->sync($validate["role"]);
+
+		$user->save();
+
+		//dd($validate);
+		return redirect(route("users.index"))->with(
+			"Suscess",
+			"sussesfully created"
+		);
 		//
 	}
 
@@ -48,14 +76,32 @@ class UserController extends Controller {
 	/**
 	 * Update the specified resource in storage.
 	 */
-	public function update(Request $request, User $role) {
+	public function update(Request $request, User $user) {
+		$request->validate([
+			"name" => "required|max:255",
+			"email" => "required|unique:users",
+			"password" => "required",
+			"role" => "required|exists:roles,name",
+		]);
+
+		$user->name = $request->name;
+		$user->email = $request->email;
+		$user->password = Hash::make($request->password);
+		$user->syncRoles($request->role);
+
+		$user->save();
+
+		return redirect(route("users.index"));
 		//
 	}
 
 	/**
 	 * Remove the specified resource from storage.
 	 */
-	public function destroy(User $role) {
+	public function destroy(User $user) {
+		$user->delete();
+
+		return redirect(route("users.index"));
 		//
 	}
 }
